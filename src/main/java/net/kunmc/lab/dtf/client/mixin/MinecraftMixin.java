de022f87f8afec71cb110d7a23af90d152440005
MORIMORI0317 <morimori.0317@outlook.jp>
2021-03-04 07:24:19 +0900

    F.D.T.H.O.H

--- src/main/java/net/kunmc/lab/dtf/client/mixin/MinecraftMixin.java
+++ src/main/java/net/kunmc/lab/dtf/client/mixin/MinecraftMixin.java
@@ -1,7 +1,6 @@
 package net.kunmc.lab.dtf.client.mixin;
 
 import net.kunmc.lab.dtf.client.renderer.WaveRenderer;
-import net.kunmc.lab.dtf.client.shader.WaveShader;
 import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
